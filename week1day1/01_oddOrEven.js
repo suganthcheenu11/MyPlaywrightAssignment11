f234871483c7aec75a0c0bf28c8daeccba807ac9
suganthcheenu11 <55043023+suganthcheenu11@users.noreply.github.com>
2023-11-26 14:10:47 +0530

    Week 1 Day 1 Assignments.

---
+++ week1day1/01_oddOrEven.js
@@ -0,0 +1,24 @@
+/*
+1. Create a function named `checkOddOrEven` that takes a number as a parameter  
+
+2. Use an `if` statement to check if the number is divisible by 2 
+    Hint: if the remainder when divided by 2 is 0, then the num is even
+
+3. Declare and initialize the variable  
+
+4. Call the function and print the result
+
+*/
+
+function checkOddOrEven(number) {
+    if (number % 2 == 0) {
+        console.log("Number is even");
+    } else {
+        console.log("Number is odd");
+    }
+}
+
+var Number = 8;
+checkOddOrEven(Number);
+checkOddOrEven(7);
+checkOddOrEven(0);
---
+++ week1day1/02_studentScore.js
@@ -0,0 +1,41 @@
+/*
+1. Create a function named `calculateGrade` that takes a student's score as a parameter.
+
+2. Use `switch` statement inside the function - 
+   - Use a `switch` statement with the condition `true`.
+   - Use `case` statements to check different score ranges and return the corresponding grade.
+
+3. Declare and initialize the variable
+
+4. Call the function and print the result
+
+
+*/
+
+
+function calculateGrade(scores) {
+
+   switch (true) {
+      case scores >= 90 && scores <= 100:
+         return "A";
+
+      case scores >= 80 && scores < 90:
+         return "B";
+      case scores >= 70 && scores < 80:
+         return "C";
+      case scores >= 60 && scores < 70:
+         return "D";
+      case scores >= 50 && scores < 60:
+         return "E";
+      case scores >= 0 && scores < 50:
+         return "F";
+      default:
+         return "No Grade found";
+   }
+
+}
+
+let score;
+
+console.log(calculateGrade(score));
+console.log(calculateGrade(70));
